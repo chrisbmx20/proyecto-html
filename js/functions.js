90a9f17e09c173a910f5e2fb8eb1ad81c7acbd57
chrisbmx20 <calvarez@fwdcostarica.com>
2024-07-16 10:24:14 -0600

    Trabajando en el Slider

---
+++ js/functions.js
@@ -0,0 +1,17 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function plusDivs(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  let i;
+  let x = document.getElementsByClassName("slide-item");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
