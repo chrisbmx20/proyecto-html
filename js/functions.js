12f8a6219d4ae4ace0509b0619c75a0755c00093
chrisbmx20 <calvarez@fwdcostarica.com>
2024-07-18 00:37:06 -0600

    video Background

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,43 @@
-
-
 let slideIndex = 1;
-showSlide(slideIndex);
+let autoSlideInterval;
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    showSlide(slideIndex);
+    startAutoSlide();
+});
+
+// Start the auto-slide
+function startAutoSlide() {
+    stopAutoSlide(); // Clear any existing intervals to prevent multiple intervals
+    autoSlideInterval = setInterval(() => {
+        plusSlide(1);
+    }, 3000); // Change the interval time as needed (3000 ms = 3 seconds)
+}
+
+// Stop the auto-slide
+function stopAutoSlide() {
+    if (autoSlideInterval) {
+        clearInterval(autoSlideInterval);
+    }
+}
+
+// Next/previous controls
 function plusSlide(n) {
-  showSlide(slideIndex += n);
-  console.log("estamos aqui");
+    slideIndex += n;
+    showSlide(slideIndex);
+    console.log("Slide changed");
+    stopAutoSlide();
+    // Restart auto-slide after a delay (e.g., 3 seconds)
+    setTimeout(startAutoSlide, 3000);
 }
 
 function showSlide(n) {
-  let i;
-  let x = document.getElementsByClassName("slide-item");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+    let i;
+    let x = document.getElementsByClassName("slide-item");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
 }
